68d322732acdac8bc6f383fd5960940198332b0f
avanthik33 <avanthik33@gmail.com>
2024-02-08 16:02:34 +0530

    .\.git\

--- connerctors/addPatientRouter.js
+++ connerctors/addPatientRouter.js
@@ -14,4 +14,10 @@ router.get("/view",async(req,res)=>{
   res.json(data)
 })
 
+router.post("/search",async(req,res)=>{
+  let data=req.body
+  let searchModel=await addPatientModel.find(data)
+  res.json(searchModel)
+})
+
 module.exports=router
